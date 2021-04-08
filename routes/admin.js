6bfb880b9fd489d9291d065b7cd94755b0de7b8a
Yasharth dubey <yashdubeywinner@gmail.com>
2021-04-09 01:52:26 +0530

    added company and student approve

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const CollegeSchema = require('../schemas/College');
-
+const CompanySchema = require('../schemas/Company');
+const StudentSchema = require('../schemas/Student');
 router.post(
     '/approvecollege',
     async (req,res) =>{
@@ -9,6 +10,28 @@ router.post(
         var data = await CollegeSchema.findOne({email : email});
         var tt = true;
         var data_s = await CollegeSchema.findByIdAndUpdate(data.id,{approve : tt});
+        res.send(data_s); 
+    }
+);
+
+router.post(
+    '/approvecompany',
+    async (req,res) =>{
+        let {email} = req.body;
+        var data = await CompanySchema.findOne({email : email});
+        var tt = true;
+        var data_s = await CompanySchema.findByIdAndUpdate(data.id,{approve : tt});
+        res.send(data_s); 
+    }
+);
+
+router.post(
+    '/approvestudent',
+    async (req,res) =>{
+        let {email} = req.body;
+        var data = await StudentSchema.findOne({email : email});
+        var tt = true;
+        var data_s = await StudentSchema.findByIdAndUpdate(data.id,{approve : tt});
         res.send(data_s); 
     }
 );
