fd4a6ce75a57fa7329062b8d8aefd88bec9ba3bf
Yasharth dubey <yashdubeywinner@gmail.com>
2021-04-24 22:29:21 +0530

    Update college.js

--- routes/college.js
+++ routes/college.js
@@ -148,8 +148,8 @@ router.get(
     async (req,res) => {
         var page = req.params.page;
         console.log("list");
-        page = (page - 1) * 10;
-        const data = await CollegeSchema.find({approve : false},{password : 0},{skip : page, limit : 10});
+        page = (page - 1) * 8;
+        const data = await CollegeSchema.find({approve : false},{password : 0},{skip : page, limit : 8});
         res.send(data);
     }
 );
